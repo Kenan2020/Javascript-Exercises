bac43da1812f65d69241c924b11942921b439152
kenan <kinan.mawlood@gmail.com>
2019-10-29 13:56:55 +0100

    29 Okt.

--- 29.10/02- Numbers & Math Object/main2.js
+++ 29.10/02- Numbers & Math Object/main2.js
@@ -1,24 +1,36 @@
 function ponus(){
     let sum =0;
 
-    for (let i = 0; i <= 100; i++){
+    for (let i = 0; i < 100; i++){
         let userNum = Math.floor(Math.random()*100 + 1);
         let luckyNum = Math.floor(Math.random()*100 + 1);
 
         let x = Math.abs(userNum - luckyNum);
 
         if(luckyNum == userNum){
-            `${sum += 100}`
+            console.log(`for the loop number: ${i}
+The user number: ${userNum}
+the luck number: ${luckyNum}
+The user wins 100$
+Total: ${sum += 100}$`)
 
         }else if(x <= 10){
-            `${sum +=(100 - x * 10)}`
+            console.log(`for the loop number: ${i}
+The user number: ${userNum}
+the luck number: ${luckyNum}
+The user wins ${100 - x * 10}$
+Total: ${sum +=(100 - x * 10)}$`)
 
         }else{
-        `${sum += 0}`
+        console.log(`for the loop number: ${i}
+The user number: ${userNum}
+the luck number: ${luckyNum}
+The user wins nothing
+Total: ${sum += 0}$`)
 
         }
     }
-    return sum
+    return `The Total is: ${sum} $`
 }
 
 
